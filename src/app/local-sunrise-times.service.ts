54358394ff14595cde8a9d34b70af6e3dd95cdf3
Blair Brown <blair.j.brown@gmail.com>
2021-01-21 23:20:55 -0500

    closer but still not displaying times

--- src/app/local-sunrise-times.service.ts
+++ src/app/local-sunrise-times.service.ts
@@ -1,23 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+interface SunriseSunsetTimes {
+  sunrise: string;
+  sunset: string;
+  daylength: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class LocalSunriseTimesService {
   //create a timeResponseObjectHere and an interface for it above.
 
+
   urlString: string =
     'https://api.sunrise-sunset.org/json?lat=42.33143&lng=-83.04575';
 
   constructor(private http: HttpClient) {}
 
+  public timesResponseObject : SunriseSunsetTimes;
+
   getSunriseSunsetTimes() {
     return this.http.get(this.urlString).subscribe(
       (data: any) => {
-          console.log("sunrise", data.results.sunrise);
-          console.log("sunrisetype", typeof (data.results.sunrise));
-          console.log("sunset", data.results.sunset);
+        const timesResponseObject: SunriseSunsetTimes = {
+          sunrise: data.results.sunrise,
+          sunset: data.results.sunset,
+          daylength: data.results.daylength,
+        };
       },
       (error) => console.log(error)
     );
